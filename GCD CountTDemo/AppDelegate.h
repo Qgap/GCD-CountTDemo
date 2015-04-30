fbbd72ddfda75a3ab37ddfd6ada6a7d3f16a0eb3
jerry_zhao <jerry_zhao@jerry-zhaodeMacBook-Air.local>
2015-04-30 18:53:03 +0800

    Initial Commit

---
+++ GCD CountTDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD CountTDemo
+//
+//  Created by jerry_zhao on 15-4-30.
+//  Copyright (c) 2015年 gq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
